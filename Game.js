2e8aee2fc5c14305c1a87e4c69d69345cc0be264
Null-Edge <nulledge.it@gmail.com>
2020-12-10 23:17:31 +0200

    Fixed Grid + background color
    the script now successfully creates a 3 by 3 grid

--- Game.js
+++ Game.js
@@ -6,22 +6,22 @@ function game_set()
     board_size = 3;//prompt("Please Enter the prefered board size \nExample: 3 is 3X3 board \n(3, 6, 9, n)","4");
     /* need to check if board size is not empty and is number first */
     var Board_Container = document.getElementById('GameContainer');
-    for (i = 0 ; i < board_size ; i++)
+    for (PIndex = 0 ; PIndex < board_size ; PIndex++)
     {
         var TBL_P = document.createElement('p'); 
         Board_Container.appendChild(TBL_P);
-        for (j = 0 ; j < board_size ; j++)
+        for (TBLIndex = 0 ; TBLIndex < board_size ; TBLIndex++)
         {   //loop creates dynamic number of tables
             var TBL = document.createElement('table');
             TBL_P.appendChild(TBL);
-            TBL.style.border = '1px solid black';
-            for (var i = 0; i < 3; i++) 
+            for (var j = 0; j < 3; j++) 
             {   //loop creates 3 rows 
                 var tbl_row = document.createElement('tr');  
                 TBL.appendChild(tbl_row);
                 for (var i = 0; i < 3; i++) 
                 {   //loop creates 3 cells
                     var TData = document.createElement('td');
+                    TData.width ='75';
                     tbl_row.appendChild(TData);
                 } 
             }
